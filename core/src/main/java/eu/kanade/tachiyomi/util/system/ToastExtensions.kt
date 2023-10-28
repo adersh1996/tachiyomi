4f4aaa9d67b0384e00a3cb076ebc3c710daaf4ec
adersh1996 <adersh1996@gmail.com>
2023-10-28 22:22:06 +0530

    adersh1996:toast_message_display_weird_#9964

--- core/src/main/java/eu/kanade/tachiyomi/util/system/ToastExtensions.kt
+++ core/src/main/java/eu/kanade/tachiyomi/util/system/ToastExtensions.kt
@@ -4,18 +4,24 @@ import android.content.Context
 import android.widget.Toast
 import androidx.annotation.StringRes
 
+
 /**
  * Display a toast in this context.
  *
  * @param resource the text resource.
  * @param duration the duration of the toast. Defaults to short.
  */
+
 fun Context.toast(
     @StringRes resource: Int,
     duration: Int = Toast.LENGTH_SHORT,
-    block: (Toast) -> Unit = {},
+    block: (Toast) -> Unit = {}
 ): Toast {
-    return toast(getString(resource), duration, block)
+    val text = getString(resource)
+    val toast = Toast.makeText(applicationContext, text, duration)
+    block(toast)
+    toast.show()
+    return toast
 }
 
 /**
@@ -24,13 +30,14 @@ fun Context.toast(
  * @param text the text to display.
  * @param duration the duration of the toast. Defaults to short.
  */
+
 fun Context.toast(
     text: String?,
     duration: Int = Toast.LENGTH_SHORT,
-    block: (Toast) -> Unit = {},
+    block: (Toast) -> Unit = {}
 ): Toast {
-    return Toast.makeText(applicationContext, text.orEmpty(), duration).also {
-        block(it)
-        it.show()
-    }
+    val toast = Toast.makeText(applicationContext, text.orEmpty(), duration)
+    block(toast)
+    toast.show()
+    return toast
 }
